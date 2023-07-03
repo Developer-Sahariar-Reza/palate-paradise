e920d8d8398bc87b4e073022fca9b2797cca8a25
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-07-03 20:31:52 +0600

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container mb-10">
+      <h1 className="text-center font-bold header-text text-xl">
+        © 2023 Palace Paradise. All rights reserved.
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
